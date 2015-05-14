3e93e14307ff92241654ec0c27aad832ce959b36
Gian Carlo Z. Asuncion <asunciongiancarlo@gmail.com>
2015-05-14 12:38:28 +0800

    Dashboard update: require active user
    Please update your routes.php at this line:
    Ex. ['middleware'=>'is.allowed']
    
    Route::resource('general_settings', 'CMS\GeneralSettingsController',
    ['middleware'=>'is.allowed']);

--- app/Http/Controllers/CMS/NewsFeedsController.php
+++ app/Http/Controllers/CMS/NewsFeedsController.php
@@ -5,8 +5,19 @@ use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 Use Feeds;
+use View;
 
 class NewsFeedsController extends Controller {
+
+	public function __construct()
+	{
+		//Read the settings .env set app title and tag line
+		View::share('APP_TITLE', env('APP_TITLE'));
+		View::share('APP_TAG_LINE', env('APP_TAG_LINE'));
+
+		$this->middleware('guest'); 	  //Doesn't require active user
+		//$this->middleware('is.allowed'); //Require require active user
+	}
 
 	public function index()
 	{
