08bb6b12ff7bcaff43ec34531c808b377d2b3151
Charmaine Pineda <cpineda@yondu.com>
2015-05-21 10:48:04 +0800

    Signed-off-by: CHA

--- resources/views/cms/user/profile.blade.php
+++ resources/views/cms/user/profile.blade.php
@@ -7,9 +7,8 @@
     <div class="row">
        <div class="col-sm-11 my-profile">
             {!! HTML::link('cms/user/update','Update Profile', array('class' => 'btn btn-add pull-left')) !!}            
-            <button type="button" class="btn btn-reset pull-right" data-toggle="modal" data-target="#changepassword">
-              Change Password
-            </button>
+            {!! Form::button('changepass', array('class' => 'btn btn-reset pull-right', 'data-toggle' => 'modal', 'data-target' => '#changepassword')) !!}
+                        
        </div>
    </div>
 </div>
