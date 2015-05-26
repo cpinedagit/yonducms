281566f466b9d9d89f779ea3a4d4361d6a94ebcd
Charmaine Pineda <cpineda@yondu.com>
2015-05-26 10:42:15 +0800

    Signed-off-by: Charmaine Pineda <cpineda@yondu.com>

--- app/Http/breadcrumbs.php
+++ app/Http/breadcrumbs.php
@@ -155,12 +155,7 @@ Breadcrumbs::register('cms.general_settings.index', function($breadcrumbs)
     $breadcrumbs->parent('cms.index');
     $breadcrumbs->push('General Settings', route('cms.general_settings.index'));
 });
-<<<<<<< HEAD
-        
-        
-=======
 
->>>>>>> origin/master
 Breadcrumbs::register('cms.user.edit', function($breadcrumbs)
 {
     $breadcrumbs->parent('cms.user.index');
@@ -180,8 +175,6 @@ Breadcrumbs::register('cms.general_settings.index', function($breadcrumbs)
     $breadcrumbs->parent('cms.index');
     $breadcrumbs->push('General Settings', route('cms.general_settings.index'));
 });
-<<<<<<< HEAD
-
 
 //Home > Banner
 Breadcrumbs::register('cms.banners.index', function($breadcrumbs)
@@ -210,5 +203,3 @@ Breadcrumbs::register('cms.pages.addPage', function($breadcrumbs)
     $breadcrumbs->parent('cms.pages.index');
     $breadcrumbs->push('Add New', route('cms.pages.addPage'));
 });
-=======
->>>>>>> origin/master
