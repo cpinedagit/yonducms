dce0e5f197d799bdb1efbeb92d60114c64b28644
Gian Carlo Z. Asuncion <asunciongiancarlo@gmail.com>
2015-05-26 09:55:14 +0800

    breadcrumbs

--- app/Http/breadcrumbs.php
+++ app/Http/breadcrumbs.php
@@ -155,12 +155,7 @@ Breadcrumbs::register('cms.general_settings.index', function($breadcrumbs)
     $breadcrumbs->parent('cms.index');
     $breadcrumbs->push('General Settings', route('cms.general_settings.index'));
 });
-<<<<<<< HEAD
-        
-        
-=======
 
->>>>>>> origin/master
 Breadcrumbs::register('cms.user.edit', function($breadcrumbs)
 {
     $breadcrumbs->parent('cms.user.index');
@@ -180,7 +175,6 @@ Breadcrumbs::register('cms.general_settings.index', function($breadcrumbs)
     $breadcrumbs->parent('cms.index');
     $breadcrumbs->push('General Settings', route('cms.general_settings.index'));
 });
-<<<<<<< HEAD
 
 
 //Home > Banner
@@ -210,5 +204,4 @@ Breadcrumbs::register('cms.pages.addPage', function($breadcrumbs)
     $breadcrumbs->parent('cms.pages.index');
     $breadcrumbs->push('Add New', route('cms.pages.addPage'));
 });
-=======
->>>>>>> origin/master
+
