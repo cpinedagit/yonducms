9205b0bf8f8e36102ba59774ee397072a907162e
Charmaine Pineda <cpineda@yondu.com>
2015-06-24 11:26:38 +0800

    Beam styles and scripts Signed-off-by: Charmaine Pineda <cpineda@yondu.com>

--- public/beam/js/main.js
+++ public/beam/js/main.js
@@ -6,7 +6,94 @@ setTimeout(function(){
             element.addClass('schedule__list--active');
         })
     })
-    $(".slick-next,.slick-prev").click(function(){
+   /* $(".slick-next,.slick-prev").click(function(){
         $('.schedule__list--active').removeClass('schedule__list--active');
+    });*/
+    
+    var repetitionclick=0;
+    $('.thumbnails .slick-slide').each(function(){
+    
+        $(this).on('click',function(){
+            $('.modal-wrapper').fadeIn();
+            $('body').addClass('adjustment-div');
+            if(repetitionclick === 0){
+                 $('.modal-slider').slick({
+                      dots: false,
+                      infinite: true,
+                      speed: 300,
+                      slidesToShow: 1,
+                      centerMode: true,
+                      adaptiveHeight: true
+                  });
+                repetitionclick++;
+            }
+            var eq=$(this).attr('data-slick-index');
+            $('.modal-slider').slick('slickGoTo', parseInt(eq))
+        });
     });
+    
+    $('.modal-close').click(function(){
+        $(this).closest('.modal-wrapper').fadeOut();
+        $('body').removeClass('adjustment-div');
+    });
+    
+    /*$('.accordion-updates').on('show.bs.collapse', function () {
+        $('.accordion-updates .panel-heading')
+    })*/
+    
+    $('.accordion-updates .collapse').each(function(){
+        $(this).on('show.bs.collapse', function () {
+            $(this).prev().addClass('active');
+        });
+        $(this).on('hide.bs.collapse', function () {
+            $(this).prev().removeClass('active');
+        });
+    });
+    
+    $('.contact-us-address-list li a').each(function(){
+        $(this).click(function(){
+            var element = this;
+            var target = $(this).attr('data-target');
+            $('.contact-us-address-list li.active').removeClass('active');
+            
+            $('.map-holder').removeClass('active');
+            setTimeout(function(){
+                 $('div[data-place="'+target+'"]').addClass('active');
+                $(element).parent().addClass('active');
+            },200);
+           
+        });   
+    });
+        
+        $('a[data-toggle="modal"]').each(function(){
+            $(this).on('click',function(){
+                var target='#'+$(this).attr('data-target');
+                $(target).fadeIn();
+            });
+        });
+    
+    $('.sched-main-time__day__dropdown').change(function(){
+        var val=$(this).val();
+        
+        $('.sched-main-time__time__sched__list.active').removeClass('active');
+        setTimeout(function(){
+            $('#'+val).addClass('active');
+        },200)
+    })
+    
+    var d = new Date();
+    var n = d.getDay() - 1;
+    $(".sched-main-time__day__dropdown").prop('selectedIndex', n);
+    setTimeout(function(){
+         var val=$(".sched-main-time__day__dropdown").val();
+        
+        $('.sched-main-time__time__sched__list.active').removeClass('active');
+        setTimeout(function(){
+            $('#'+val).addClass('active');
+        },200)
+    },200);
+   
+        
+   
 },500);
+
--- resources/views/site/partials/scripts.blade.php
+++ resources/views/site/partials/scripts.blade.php
@@ -1,30 +1,10 @@
-{!!HTML::script('public/site/js/jquery.min.js') !!}
-<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
-{!! HTML::script('public/site/js/bootstrap.min.js'); !!}
-
-{!! HTML::script('public/beam/js/bootstrap.min.js') !!}
-{!! HTML::script('public/beam/js/slick.js') !!}
-{!! HTML::script('public/beam/js/main.js') !!}
-
-<!--this scripts are used for page management-->
-
-{!!HTML::script('public/site/js/plugins.js') !!}
-{!! HTML::script('public/site/js/transformicon.js') !!}
-{!!HTML::script('public/site/js/main.js') !!}
-{!!HTML::script('public/site/js/menu/app.js') !!}
-{!!HTML::script('public/site/js/menu/main.js') !!}
-{!!HTML::script('public/site/js/menu/core.min.js') !!}
-{!!HTML::script('public/site/js/menu/metisMenu.min.js') !!}
-{!! HTML::script('public/site/js/vendor/modernizr-2.8.3.min.js') !!}
-<script>transformicons.add('.tcon')</script>
-<script src="http://maps.googleapis.com/maps/api/js"></script>
-
-
 <!-- Standard Banner-->
-{!! HTML::script('public/js/beam/ekko-lightbox.min.js') !!}
-{!! HTML::script('public/js/beam/modernizr.js') !!}
-{!! HTML::script('public/js/beam/ekko-lightbox.min.js') !!}
-{!! HTML::script('public/js/beam/carousel.js') !!}
+<script src="js/vendor/modernizr-2.8.3.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+{!! HTML::script('public/scheduler/js/bootstrap.min.js') !!}
+{!! HTML::script('public/scheduler/js/slick.js') !!}
+{!! HTML::script('public/scheduler/js/main.js') !!}
 <!-- Standard Banner-->
 
  <script>
